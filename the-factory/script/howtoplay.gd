48ddec28dcfc55c297b91ea45d83c34152c69c7f
ait1m <68070042@kmitl.ac.th>
2025-10-31 21:45:33 +0700

    link page

--- the-factory/script/howtoplay.gd
+++ the-factory/script/howtoplay.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_goback_pressed() -> void:
-	get_tree().change_scene_to_file("res://main menu.tscn")
+	get_tree().change_scene_to_file("res://scene/main menu.tscn")
