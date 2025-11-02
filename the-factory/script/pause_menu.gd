094fdebd8a8a582f0ef4fd588ad556c5180a6c36
Boiiidiwa <boonsita.nbr@gmail.com>
2025-11-03 03:28:41 +0700

    add paues menu map345

--- the-factory/script/pause_menu.gd
+++ the-factory/script/pause_menu.gd
@@ -23,4 +23,4 @@ func _on_replay_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func _on_quit_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scene/level_select.tscn")
