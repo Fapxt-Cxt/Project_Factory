2ca558ad2c169198291dc9a83015115ede552209
Boiiidiwa <boonsita.nbr@gmail.com>
2025-11-03 15:38:02 +0700

    edit coding pause page

--- the-factory/script/pause_menu.gd
+++ the-factory/script/pause_menu.gd
@@ -23,4 +23,5 @@ func _on_replay_pressed() -> void:
 	get_tree().reload_current_scene()
 
 func _on_quit_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scene/level_select.tscn")
